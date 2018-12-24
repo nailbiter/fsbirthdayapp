840ee8c74fc3fae3dfff41cf228321a81efbfb06
Alex Leontiev <alozz1991@gmall.com>
2018-12-24 14:56:18 +0900

    birthday reminder bot files changed:  M index.html  M script.js
    trello card: https://trello.com/c/E4Ei2Mbz

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '287927608592313',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v3.2'
-    });
-  };
+		window.fbAsyncInit = function() {
+		FB.init({
+			 appId            : '287927608592313',
+			 autoLogAppEvents : true,
+			 xfbml            : true,
+			version          : 'v3.2'
+		});
+		FB.login(function(response) {
+				if (response.authResponse) {
+				 console.log('Welcome!  Fetching your information.... ');
+				 FB.api('/me', function(response) {
+					 console.log('Good to see you, ' + response.name + '.');
+				 });
+				} else {
+				 console.log('User cancelled login or did not fully authorize.');
+				}
+		});
+		};
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+		(function(d, s, id){
+			 var js, fjs = d.getElementsByTagName(s)[0];
+			 if (d.getElementById(id)) {return;}
+			 js = d.createElement(s); js.id = id;
+			 js.src = "https://connect.facebook.net/en_US/sdk.js";
+			 fjs.parentNode.insertBefore(js, fjs);
+		 }(document, 'script', 'facebook-jssdk'));
